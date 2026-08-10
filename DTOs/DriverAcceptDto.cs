46df0f61d7f214ad7d945eb7ea91598f95b86c3d
saboagye5678-ctrl <saboagye5678@gmail.com>
2026-08-10 08:48:08 +0000

    Delete DTOs directory

--- DTOs/DriverAcceptDto.cs
+++
@@ -1,8 +0,0 @@
-namespace CampusRide.API.DTOs;
-
-public class DriverAcceptDto
-{
-    public string DriverId { get; set; } = string.Empty;
-
-    public string DriverName { get; set; } = string.Empty;
-}
